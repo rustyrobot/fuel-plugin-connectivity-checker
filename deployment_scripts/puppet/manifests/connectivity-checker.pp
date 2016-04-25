0b7551637cc0c650b227e6190d30cdc3446144b3
Sergey Vasilenko <svasilenko@mirantis.com>
2016-04-25 17:24:35 -0500

    Initial implementation, based on threads inside puppet
    and external ping utility.

---
+++ deployment_scripts/puppet/manifests/connectivity-checker.pp
@@ -0,0 +1,17 @@
+notice('MODULAR: connectivity-checker.pp')
+
+$plugin_name      = 'connectivity_checker'
+$plugin_settings  = hiera_hash("${plugin_name}", {})
+$task_deploy      = hiera('task_deploy', false)
+
+if $task_deploy {
+    connectivity_checker { 'netconfig':
+      network_scheme   => hiera_hash('network_scheme'),
+      network_metadata => hiera_hash('network_metadata'),
+      non_destructive  => pick($plugin_settings['non_destructive'], false),
+      ping_tries       => pick($plugin_settings['ping_tries'], 5),
+      ping_timeout     => pick($plugin_settings['ping_timeout'], 20),
+    }
+} else {
+  warning("*** Connectivity check can't be processed because task-based deployment disabled. ***")
+}
